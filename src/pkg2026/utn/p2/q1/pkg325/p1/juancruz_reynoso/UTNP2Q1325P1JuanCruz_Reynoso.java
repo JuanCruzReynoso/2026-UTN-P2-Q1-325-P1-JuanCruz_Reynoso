6c2127c844734a9ca6729649cd4b0e81999475c1
JuanCruzReynoso <juanosk898@gmail.com>
2026-05-22 17:02:08 -0300

    chore(init): inicializar proyecto base de NetBeans y estructura del parcial

---
+++ src/pkg2026/utn/p2/q1/pkg325/p1/juancruz_reynoso/UTNP2Q1325P1JuanCruz_Reynoso.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2026.utn.p2.q1.pkg325.p1.juancruz_reynoso;
+
+/**
+ *
+ * @author ChillOutKet
+ */
+public class UTNP2Q1325P1JuanCruz_Reynoso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
